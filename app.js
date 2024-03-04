cda2c1ced0ab0c77b61e2429b6c42058eea45283
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 19:30:38 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import presentDrinks from "./src/presentDrinks.js";
-const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=azzzzzz";
 
 window.addEventListener("DOMContentLoaded", ()=> {
     presentDrinks(URL)
--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -3,6 +3,11 @@ import get from "./getElement.js"
 const displayDrinks = ({drinks}) => {
     const section = get(".section-center");
     const title = get(".title");
+    if(!drinks){
+        title.textContent = "sorry, no drinks matched your search";
+        section.innerHTML = null;
+        return;
+    }
 }
 
 export default displayDrinks;
