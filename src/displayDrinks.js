449362115073ea50d7bb17396cc8306d753a1275
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 19:21:49 +0430

    adding some changes

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -2,7 +2,7 @@ import get from "./getElement.js"
 
 const displayDrinks = ({drinks}) => {
     const section = get(".section-center");
-    const section = get(".section-center");
+    const title = get(".title");
 }
 
 export default displayDrinks;
