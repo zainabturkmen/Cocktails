07cbe550da207e9bf87426ba47f87df61f81ce29
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 19:21:23 +0430

    adding some changes

---
+++ src/displayDrinks.js
@@ -0,0 +1,8 @@
+import get from "./getElement.js"
+
+const displayDrinks = ({drinks}) => {
+    const section = get(".section-center");
+    const section = get(".section-center");
+}
+
+export default displayDrinks;
--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,11 +1,12 @@
 import fetchDrinks from "./fetchDrinks.js";
-
+import displayDrinks from "./displayDrinks.js";
 const showDrinks = async (url) => {
     // fetch drinks
     const data = await fetchDrinks(url)
 
     // display drinks
-    const section = await displyDrinks(data);
+    const section = await displayDrinks(data);
+    console.log(section);
 };
 
 export default showDrinks;
