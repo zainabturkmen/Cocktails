0b98567b35edd92b767d31ceaab745af2912c0bd
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 19:58:22 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import presentDrinks from "./src/presentDrinks.js";
+import "./src/searchForm.js"
 const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
 
 window.addEventListener("DOMContentLoaded", ()=> {
---
+++ src/searchForm.js
@@ -0,0 +1,6 @@
+import get from "./getElement.js";
+import presentDrinks from "./presentDrinks.js"
+
+const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+
