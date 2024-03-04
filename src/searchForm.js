44c91487eb30af2601e173451eb792d035116952
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 20:03:33 +0430

    adding some changes

--- src/searchForm.js
+++ src/searchForm.js
@@ -2,5 +2,10 @@ import get from "./getElement.js";
 import presentDrinks from "./presentDrinks.js"
 
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+ 
+const form = get(".search-form");
+const input = get('[name="drink"]');
 
-
+form.addEventListener("keyup", (e)=>{
+e.preventDefault();
+})
