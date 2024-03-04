25f0551c4ea44c49a63647bcd5f7cd90bd7bc882
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 21:59:18 +0430

    adding some changes

--- src/toggleLoading.js
+++ src/toggleLoading.js
@@ -3,7 +3,7 @@ import get from "./getElement.js";
 const loading = get(".loading");
 
 export  const showLoading = () => {
-    loading.classList.remove("hide-loading")
+    loading.classList.remove("hide-loading");
 };
 export  const hideLoading = () => {
     loading.classList.add("hide-loading")
