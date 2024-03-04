73daa38e4f7201acea784d011e93cfb3fd2ea86e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-04 20:30:42 +0430

    adding some changes

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -1,9 +1,11 @@
 import get from "./getElement.js"
-
+import {hideLoading} from "./toggleLoading.js"
 const displayDrinks = ({drinks}) => {
     const section = get(".section-center");
     const title = get(".title");
     if(!drinks){
+        // hide loading
+        hideLoading();
         title.textContent = "sorry, no drinks matched your search";
         section.innerHTML = null;
         return;
--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,6 +1,6 @@
 import {showLoading} from "./toggleLoading.js"
 const fetchDrinks = async (url) => {
-    
+    showLoading()
     try {
         const response = await fetch(url);
         const data = await response.json(); 
