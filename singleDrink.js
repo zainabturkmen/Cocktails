757bef3a6910958c6b1d6d0209d2db3d20692369
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 07:05:37 +0430

    adding some changes

--- singleDrink.js
+++ singleDrink.js
@@ -5,10 +5,11 @@ import displayDrink from "./src/displaySingleDrink.js";
 const presentDrink = async () => {
     const id = localStorage.getItem("drink");
     if(!id){
-        window.location.replace()
+        window.location.replace("index.html")
+    }else{
+        const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
     }
-    const drink = fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    displayDrink(drink)
+    displayDrink(drink);
 };
 
 window.addEventListener("DOMContentLoaded", presentDrink);
