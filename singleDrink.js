2db637abe38a9725ad243f784b8f86c675e6ae4a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 07:05:59 +0430

    adding some changes

--- singleDrink.js
+++ singleDrink.js
@@ -8,8 +8,8 @@ const presentDrink = async () => {
         window.location.replace("index.html")
     }else{
         const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    }
-    displayDrink(drink);
+        displayDrink(drink);
+    };
 };
 
 window.addEventListener("DOMContentLoaded", presentDrink);
