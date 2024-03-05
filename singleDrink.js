ffaa7ca285d9931a40c445fddde265b8e98e843e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 06:54:46 +0430

    adding some changes

--- singleDrink.js
+++ singleDrink.js
@@ -3,6 +3,7 @@ import fetchDrinks  from "./src/fetchDrinks.js";
 import displayDrinks from "./src/displaySingleDrink.js";
 
 const presentDrink = async () => {
+    const id = localStorage.getItem("drink");
     fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=`)
 };
 
