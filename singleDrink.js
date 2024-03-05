f276e8bcdccd649724fc82164ac2030fc8a15d12
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 06:47:03 +0430

    adding some changes

--- singleDrink.js
+++ singleDrink.js
@@ -3,7 +3,7 @@ import fetchDrinks  from "./src/fetchDrinks.js";
 import displayDrinks from "./src/displaySingleDrink.js";
 
 const presentDrink = async () => {
-
+    fetch()
 };
 
 window.addEventListener("DOMContentLoaded", presentDrink);
