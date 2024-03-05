8a53a640a1d3798de801fd42d95bac1bb5fab7da
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 07:12:58 +0430

    adding some changes

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -1,10 +1,11 @@
 import {hideLoading} from "./toggleLoading.js";
 import get from "./getElement.js";
 
-const displayDrinks = (drink) => {
+const displayDrinks = (data) => {
     hideLoading();
-    
-    console.log(drink);
+
+    const dirnk = data.dirnks[0];
+    const {strDrinkThumb:image, strDrink:name, strInstructions} = data
 };
 
 export default displayDrinks;
