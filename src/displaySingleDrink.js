4a0a859830a5b2544de2fa31d1f8e63746173e26
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 07:20:08 +0430

    adding some changes

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -8,10 +8,10 @@ const displayDrinks = (data) => {
     const {strDrinkThumb:image, strDrink:name, strInstructions:desc} = drink;
     const list = [
         drink.strIngredient1,
-        drink.strIngredient1,
-        drink.strIngredient1,
-        drink.strIngredient1,
-        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
     ]
 };
 
